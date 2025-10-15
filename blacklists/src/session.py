95597c337581315e4058a6da80bd88f25cdfc95a
garciajosd_ghco <garciajosd@globalhitss.com>
2025-10-15 11:33:30 -0500

    Change db_host

--- blacklists/src/session.py
+++ blacklists/src/session.py
@@ -10,11 +10,10 @@ class SessionConfig():
     def url(self):
         db_user = 'postgres'
         db_pass = 'postgres'
-        db_host = 'blacklists-postgres.cxewsuko42nv.us-east-1.rds.amazonaws.com'
+        db_host = 'blacklist-postgres.cxi4kauewdcn.us-east-2.rds.amazonaws.com'
         db_port = 5432
         db_name = 'db_blacklist'
         return f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}'
-
 
 session_config = SessionConfig()
 engine = create_engine(session_config.url())
